e08a06649ca07d7762641a497f5bb0f1b7cc3cb4
Matt Chisholm <matt@theory.org>
2013-02-16 11:47:20 -0800

    run out of virtualenv

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/matt/neighborhood/
 cp map.log map.log.old
-nice python map.py | grep neighborhood > map.log
+nice /home/matt/.virtualenvs/neighborhoodenv/bin/python map.py | grep neighborhood > map.log
 
 if [ "x`diff map.log map.log.old`" != "x" ]; then 
     echo 'map.py found some new points today.'; 
@@ -9,3 +9,6 @@ else
     echo 'map.py did not find any new points today.';
     cat map.log;
 fi
+
+/home/matt/.virtualenvs/neighborhoodenv/bin/python scrape.py start
+
